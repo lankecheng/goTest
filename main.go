b4ffde458c68432e46b46826e6b18e73dfd83366
lankc <lkc@meitu.com>
2015-06-19 16:32:04 +0800

    七七八八先提交

--- main.go
+++ main.go
@@ -15,9 +15,72 @@ import (
 	//"net/http"
 	"reflect"
 	"strings"
+	"encoding/binary"
+	"os"
+	"os/exec"
+	"github.com/kardianos/osext"
+	"path/filepath"
+	"path"
+	"runtime"
 )
 
 func main() {
+	var i_a int = 3
+
+	if float64(i_a) > 1.1 {
+		fmt.Println(i_a)
+	}
+	fmt.Println("-------")
+	fmt.Println(os.Args[0])
+	fmt.Println("-------")
+	a, _ := filepath.Abs("./logs")
+	fmt.Println("test:", a)
+	_, currFile, _, _ := runtime.Caller(0)
+	fmt.Println(currFile)
+	fmt.Println("-------")
+	file, _ := os.Getwd()
+	fmt.Printf("current path: %v\n", file)
+	file, _ = exec.LookPath(os.Args[0])
+	args0, _ := filepath.Abs(file)
+	fmt.Printf("args[0]: %v\n", file)
+	fmt.Printf("args[0] path: %v\n", args0)
+	fmt.Printf("args[0] dir: %v\n", path.Dir(args0))
+	logDir, err := filepath.Abs(path.Dir(args0) + "/..")
+	if err == nil {
+		fmt.Printf("log dir: %v\n", logDir)
+	} else {
+		fmt.Printf("log dir: %v\n", err)
+	}
+
+	fmt.Printf("tmp path: %v\n", os.TempDir())
+
+	filename, _ := osext.Executable()
+//	binFolder := path.Dir(filename);
+	fmt.Println(filename)
+
+	fmt.Println("-------")
+
+	defer func() {
+		if err := recover(); err != nil {
+			fmt.Println("Recovered ", err)
+		}
+	}()
+
+	fmt.Println([]byte("abc"))
+	fmt.Println("-------")
+	var test1 int64
+	var test2 uint64 = 1
+	test1 = int64(test2)
+	fmt.Println(test1)
+	fmt.Println("-------")
+	var i64 int64 = 256
+	i64LtlB := make([]byte, 8)
+	i64bigB := make([]byte, 8)
+	binary.LittleEndian.PutUint64(i64LtlB, uint64(i64))
+	binary.BigEndian.PutUint64(i64bigB, uint64(i64))
+	fmt.Println(i64LtlB)
+	fmt.Println(i64bigB)
+	fmt.Println("-------")
 	b := "abc123"
 	fmt.Println(b[0], reflect.TypeOf(b[0]))
 
@@ -59,6 +122,8 @@ func main() {
 		fmt.Println(t)
 	}
 
+	panic("123")
+	fmt.Println("after panic")
 	/**
 	file, _ := os.Getwd()
 	fmt.Println("current path:", file)
@@ -78,16 +143,6 @@ func main() {
 	args := []string{"a", "b"}
 	c(args[0:])
 	**/
-}
-
-func test2(arg string) (filePath []string) {
-	fmt.Println(filePath == nil)
-	return
-}
-
-func test(err chan error) {
-	time.Sleep(time.Second * 2)
-	err <- fmt.Errorf("just have a test!")
 }
 
 func chg(r []rune) {
